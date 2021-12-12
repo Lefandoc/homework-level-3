fe8b97ea7c12d74803ebc7f6ec53acf7688c8267
Lefandoc <n.markelovv@gmail.com>
2021-12-12 19:47:19 +0300

    Выполнил домашнее задание к уроку 7 Reflection API

---
+++ java/ru/gb/homework7/BeforeSuite.java
@@ -0,0 +1,12 @@
+package ru.gb.homework7;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+public @interface BeforeSuite {
+
+}
